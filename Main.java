cfa0ff1f43d3cd25e6e4e4387efcdc46d83f55a7
Antonio <antonio.adolfo.jimenez@estudiante.epsum.school>
2024-10-16 11:29:27 +0200

    El for

--- Main.java
+++ Main.java
@@ -1,14 +1,9 @@
 public class Main {
 	public static void main (String[] args){
-		if (args.length() == 0){
-			System.out.println("No hay argumentos.");
-		} else {
-			System.out.println("Hay " + args.length() + " argumentos");
+		Arraylist<String> array = new Arraylist<String>();
+			array.add(args);
+			for (String arg:array){
+				System.out.println(arg);
+			}
 		}
-		
-		for (int i = 0; i <= args.length(); i++){
-			System.out.println(args);
-		}
-
 	}
-}
